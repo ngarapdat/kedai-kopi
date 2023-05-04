0b4db0e70d78f834d0a700bf6dbbc1ac4cd16e4f
ngarapdatmanik <ngarapdatmanik@gmail.com>
2023-05-04 18:20:05 +0700

    membuat website kedai kopi

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const navbarNav = document.querySelector(".navbar-nav");
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+// klik diluar menghilangkan navbar
+
+const hamburger = document.querySelector("#hamburger-menu");
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
